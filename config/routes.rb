bf9d5e693c35c940f67b5cdb83d272fe13211e09
Ravi P <ravip0711@gmail.com>
2016-01-30 16:42:42 -0700

    finished routes and validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'welcome#index'
+  get 'login' => "sessions#new"
+  post 'login' => "sessions#create"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

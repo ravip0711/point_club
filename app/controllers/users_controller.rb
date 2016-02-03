b64eae63433985c6ffacc1c07d7a99955642d648
Ravi P <ravip0711@gmail.com>
2016-02-03 10:51:03 -0700

    finished sessions was working on remember user cookies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       flash[:success] = "Welcome to the Point Club!"
       redirect_to @user
     else
